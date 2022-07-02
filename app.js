b6c8b93ef489593d0cf5a71f64265fd25aa2d441
John Solly <jsolly@protonmail.com>
2022-07-02 12:13:06 -0700

    update html filename in node

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'javascript-template.html'));
 });
 app.listen(process.env.PORT || 4000, function () {
     console.log('Node app is working!');
