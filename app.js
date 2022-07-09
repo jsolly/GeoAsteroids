42c78ef16bbc35242e030f1bdd8d1c8b270b31a4
John Solly <jsolly@protonmail.com>
2022-07-09 15:53:24 -0700

    wip

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Node app is working!');
 });
