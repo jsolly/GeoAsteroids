d2950a28c825415ac704bd74db1573bb0c3bd386
John Solly <jsolly@protonmail.com>
2022-07-09 15:50:24 -0700

    add favicon

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // Importing modules
 const express = require('express');
 const path = require('path');
+var favicon = require('serve-favicon')
 const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/home.html'));
 });
 
+
+app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(3000, () => {
