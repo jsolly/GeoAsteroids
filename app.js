f6955c492db6fd48551ec0362be6a507a358d833
John Solly <jsolly@protonmail.com>
2022-07-12 19:05:09 -0700

    Remove favicon. Change to index.html. Add port logging.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Importing modules
 const express = require('express');
 const path = require('path');
-var favicon = require('serve-favicon')
 const app = express();
 
 // set up rate limiter: maximum of five requests per minute
-var rateLimit = require('express-rate-limit')
+var rateLimit = require('express-rate-limit');
+const { listenerCount } = require('process');
 var limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
@@ -15,13 +15,12 @@ var limiter = rateLimit({
 app.use(limiter);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/home.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 
-app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(process.env.PORT || 4000, function () {
-    console.log('Node app is working!');
+var listener = app.listen(process.env.PORT || 4000, function () {
+    console.log('Node app is working on port ' + listener.address().port);
 });
