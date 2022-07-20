fb5e9518d8aa7fc8712073f724118dcff3158add
John Solly <jsolly@protonmail.com>
2022-07-19 17:24:03 -0700

    add linted files

--- built/keybindings.js
+++ built/keybindings.js
@@ -1,29 +1,29 @@
-import { ship } from './ship.js';
-import { TURN_SPEED, FPS } from './constants.js';
-import { shootLaser } from './lasers.js';
+import {ship} from './ship.js';
+import {TURN_SPEED, FPS} from './constants.js';
+import {shootLaser} from './lasers.js';
 /**
  *
  * @param {string} ev - Event fired when key is pressed down
  * @return {boolean} False if ship is dead @todo I think this is wrong
  */
 function keyDown(/** @type {KeyboardEvent} */ ev) {
-    if (ship.dead) {
-        return false;
-    }
-    switch (ev.keyCode) {
-        case 32: // Shoot laser
-            shootLaser();
-            break;
-        case 37: // left arrow (rotate ship left)
-            ship.rot = ((-TURN_SPEED / 180) * Math.PI) / FPS;
-            break;
-        case 38: // up arrow (thrust the ship forward)
-            ship.thrusting = true;
-            break;
-        case 39: // right arrow (rotate ship right)
-            ship.rot = ((TURN_SPEED / 180) * Math.PI) / FPS;
-            break;
-    }
+  if (ship.dead) {
+    return false;
+  }
+  switch (ev.keyCode) {
+    case 32: // Shoot laser
+      shootLaser();
+      break;
+    case 37: // left arrow (rotate ship left)
+      ship.rot = ((-TURN_SPEED / 180) * Math.PI) / FPS;
+      break;
+    case 38: // up arrow (thrust the ship forward)
+      ship.thrusting = true;
+      break;
+    case 39: // right arrow (rotate ship right)
+      ship.rot = ((TURN_SPEED / 180) * Math.PI) / FPS;
+      break;
+  }
 }
 /**
  *
@@ -31,22 +31,22 @@ function keyDown(/** @type {KeyboardEvent} */ ev) {
  * @return {False} if ship is dead @todo I think this is wrong
  */
 function keyUp(/** @type {KeyboardEvent} */ ev) {
-    if (ship.dead) {
-        return false;
-    }
-    switch (ev.keyCode) {
-        case 32: // Allow shooting
-            ship.canShoot = true;
-            break;
-        case 37: // Release left arrow. (stop rotating ship left)
-            ship.rot = 0;
-            break;
-        case 38: // Release up arrow. (stop thrusting the ship forward)
-            ship.thrusting = false;
-            break;
-        case 39: // Release right arrow (stop rotating ship right)
-            ship.rot = 0;
-            break;
-    }
+  if (ship.dead) {
+    return false;
+  }
+  switch (ev.keyCode) {
+    case 32: // Allow shooting
+      ship.canShoot = true;
+      break;
+    case 37: // Release left arrow. (stop rotating ship left)
+      ship.rot = 0;
+      break;
+    case 38: // Release up arrow. (stop thrusting the ship forward)
+      ship.thrusting = false;
+      break;
+    case 39: // Release right arrow (stop rotating ship right)
+      ship.rot = 0;
+      break;
+  }
 }
-export { keyUp, keyDown };
+export {keyUp, keyDown};
