5c2d3447e9091f625642c0a9d6f8a25353abdbc5
John Solly <jsolly@protonmail.com>
2022-08-25 14:30:28 -0700

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 // Importing modules
-const express = require('express');
-const app = express();
-const path = require('path');
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import path from 'path';
 
 // Register View Engine
 app.set('view engine', 'ejs');
 
 // set up rate limiter: maximum of five requests per minute
-const rateLimit = require('express-rate-limit');
+import rateLimit from 'express-rate-limit';
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 50000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
@@ -15,17 +15,15 @@ const limiter = rateLimit({
 // apply rate limiter to all requests
 app.use(limiter);
 
-const listener = app.listen(process.env.PORT || 4000, function () {
-  console.log('Node app is working on port ' + listener.address().port);
-});
+app.listen(4000, () => console.log('Server Running!'));
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.sendFile('built/main.js', { root: __dirname });
   res.render('index');
 });
 
 app.use(express.static(path.join(__dirname, '/built')));
 
-app.use((req, res) => {
-  res.status(404).sendFile('views/404Page.html', { root: __dirname });
+app.use((req: Request, res: Response) => {
+  res.status(404).render('404Page.ejs');
 });
--- src/collisions.ts
+++ src/collisions.ts
@@ -1,6 +1,6 @@
 // import { destroyAsteroid, Roid } from './asteroids.js';
-import { Laser } from './lasers.js';
+// import { Laser } from './lasers.js';
 // import { Ship } from './ship.js';
-import { distBetweenPoints } from './utils.js';
+// import { distBetweenPoints } from './utils.js';
 // import { fxHit, music } from './soundsMusic.js';
 // import { newLevel } from './scoreLevelLives.js';
