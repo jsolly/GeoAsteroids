d9d4767e09c1d6f8ddacd4d8c1beea25e5a9d3f3
John Solly <jsolly@protonmail.com>
2022-08-25 00:37:51 -0700

    refactoring

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  max: 50000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
 });
 // apply rate limiter to all requests
 app.use(limiter);
