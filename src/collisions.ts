ab6d12ada73c998b6a9adff4817aeafa715fc0cd
John Solly <jsolly@protonmail.com>
2022-08-25 22:13:32 -0700

    remove empty files

--- src/collisions.ts
+++
@@ -1,6 +0,0 @@
-// import { destroyAsteroid, Roid } from './asteroids.js';
-// import { Laser } from './lasers.js';
-// import { Ship } from './ship.js';
-// import { distBetweenPoints } from './utils.js';
-// import { fxHit, music } from './soundsMusic.js';
-// import { newLevel } from './scoreLevelLives.js';
