6ba3dde073ee4db803a9fa44749197bb28fade9f
John Solly <jsolly@protonmail.com>
2022-08-26 18:21:06 -0700

    experiment

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,26 @@
 // Importing modules
-import express, { Application, Response } from 'express';
-const app: Application = express();
-import path from 'path';
+// import express, { Application, Response } from 'express';
+// const app: Application = express();
+// import path from 'path';
 
-// set up rate limiter: maximum of five requests per minute
-import rateLimit from 'express-rate-limit';
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
-});
-// apply rate limiter to all requests
-app.use(limiter);
+// // set up rate limiter: maximum of five requests per minute
+// import rateLimit from 'express-rate-limit';
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+// });
+// // apply rate limiter to all requests
+// app.use(limiter);
 
-app.listen(process.env.PORT || 4000, function () {
-  // console.log(`Node app is working on port ${listener.address().port}`);
-});
-app.use(express.static(path.join(__dirname, '../public')));
+// app.listen(process.env.PORT || 4000, function () {
+//   // console.log(`Node app is working on port ${listener.address().port}`);
+// });
+// app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (res: Response) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// app.get('/', (res: Response) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
-app.use((res: Response) => {
-  res.sendFile(path.join(__dirname, 'public/404Page.html'));
-});
+// app.use((res: Response) => {
+//   res.sendFile(path.join(__dirname, 'public/404Page.html'));
+// });
