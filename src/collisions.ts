18c0f47ee23c17c861a40719b07f3275a82cbe18
John Solly <jsolly@protonmail.com>
2022-08-30 22:25:38 -0700

    linting

--- src/collisions.ts
+++ src/collisions.ts
@@ -2,7 +2,7 @@ import { Ship, explodeShip } from './ship.js';
 import { roidBelt, destroyRoid, Roid } from './asteroids.js';
 import { music, fxHit } from './soundsMusic.js';
 import { FPS, LASER_EXPLODE_DUR } from './constants.js';
-import { newLevel, gameOver } from './scoreLevelLives.js';
+import { gameOver } from './scoreLevelLives.js';
 
 import { Laser } from './lasers.js';
 
