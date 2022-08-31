ab2568dbfd80c285898319cc80426a6720d51cb0
John Solly <jsolly@protonmail.com>
2022-08-31 11:20:46 -0700

    Add Cypress

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -2,7 +2,7 @@ import { assert, expect, test } from 'vitest';
 import { Point } from '../src/utils';
 // Edit an assertion and save to see HMR in action
 
-test('Point', () => {
+test.concurrent('Point', () => {
   const newPoint = new Point(10, 20);
   expect(newPoint.x).toBe(10);
 });
