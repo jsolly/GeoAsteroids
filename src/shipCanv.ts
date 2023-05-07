a4d651cb40137840d923d6efd6ee81c9a76a7bbe
John Solly <jsolly@umd.edu>
2023-05-07 09:37:01 -0700

    refactoring

--- src/shipCanv.ts
+++ src/shipCanv.ts
@@ -2,33 +2,41 @@ import { CTX, CVS, SHIP_SIZE } from './config.js';
 import { Ship } from './ship.js';
 
 /**
- * draw the ship's thruster on the canvas
+ * Draw the ship's thruster on the canvas
+ * @param ship - The current Ship
  */
 function drawThruster(ship: Ship): void {
   if (!ship.exploding && ship.blinkOn) {
+    const rearLeft = {
+      x:
+        CVS.width / 2 +
+        ship.r * ((2 / 3) * Math.cos(ship.a) + 0.5 * Math.sin(ship.a)),
+      y:
+        CVS.height / 2 +
+        ship.r * ((2 / 3) * Math.sin(ship.a) - 0.5 * Math.cos(ship.a)),
+    };
+
+    const rearCenter = {
+      x: CVS.width / 2 + ((ship.r * 5) / 3) * Math.cos(ship.a),
+      y: CVS.height / 2 + ((ship.r * 5) / 3) * Math.sin(ship.a),
+    };
+
+    const rearRight = {
+      x:
+        CVS.width / 2 +
+        ship.r * ((2 / 3) * Math.cos(ship.a) - 0.5 * Math.sin(ship.a)),
+      y:
+        CVS.height / 2 +
+        ship.r * ((2 / 3) * Math.sin(ship.a) + 0.5 * Math.cos(ship.a)),
+    };
+
     CTX.fillStyle = 'red';
     CTX.strokeStyle = 'yellow';
     CTX.lineWidth = SHIP_SIZE / 10;
     CTX.beginPath();
-    CTX.moveTo(
-      // rear left
-      CVS.width / 2 +
-        ship.r * ((2 / 3) * Math.cos(ship.a) + 0.5 * Math.sin(ship.a)),
-      CVS.height / 2 +
-        ship.r * ((2 / 3) * Math.sin(ship.a) - 0.5 * Math.cos(ship.a)),
-    );
-    CTX.lineTo(
-      // rear CENTER (behind ship)
-      CVS.width / 2 + ((ship.r * 5) / 3) * Math.cos(ship.a),
-      CVS.height / 2 + ((ship.r * 5) / 3) * Math.sin(ship.a),
-    );
-    CTX.lineTo(
-      // rear right
-      CVS.width / 2 +
-        ship.r * ((2 / 3) * Math.cos(ship.a) - 0.5 * Math.sin(ship.a)),
-      CVS.height / 2 +
-        ship.r * ((2 / 3) * Math.sin(ship.a) + 0.5 * Math.cos(ship.a)),
-    );
+    CTX.moveTo(rearLeft.x, rearLeft.y);
+    CTX.lineTo(rearCenter.x, rearCenter.y);
+    CTX.lineTo(rearRight.x, rearRight.y);
     CTX.closePath();
     CTX.fill();
     CTX.stroke();
@@ -36,48 +44,45 @@ function drawThruster(ship: Ship): void {
 }
 
 /**
- *
+ * Draw ship relative to its angle
  * @param ship - The current Ship
  */
 function drawShipRelative(ship: Ship): void {
-  /*
-    An overload of drawShip that doesn't ask for the position of the ship.
-    Only the angle(a)
+  const { a } = ship;
 
-    Inputs:
-    a(number) : The angle in radians
+  const nose = {
+    x: CVS.width / 2 + (4 / 3) * ship.r * Math.cos(a + 1.06),
+    y: CVS.height / 2 + (4 / 3) * ship.r * Math.sin(a + 1.06),
+  };
 
-    Outputs:
-    void
+  const rearLeft = {
+    x:
+      CVS.width / 2 +
+      ship.r * ((-1 / 3) * Math.cos(a + 1.06) + Math.sin(a + 1.06)),
+    y:
+      CVS.height / 2 +
+      ship.r * ((-1 / 3) * Math.sin(a + 1.06) - Math.cos(a + 1.06)),
+  };
 
-    */
+  const rearRight = {
+    x:
+      CVS.width / 2 +
+      ship.r * ((-1 / 3) * Math.cos(a + 1.06) - Math.sin(a + 1.06)),
+    y:
+      CVS.height / 2 +
+      ship.r * ((-1 / 3) * Math.sin(a + 1.06) + Math.cos(a + 1.06)),
+  };
 
-  const a = ship.a;
   CTX.strokeStyle = 'white';
   CTX.lineWidth = SHIP_SIZE / 20;
   CTX.beginPath();
-  CTX.moveTo(
-    // nose of ship
-    CVS.width / 2 + (4 / 3) * ship.r * Math.cos(a + 1.06),
-    CVS.height / 2 + (4 / 3) * ship.r * Math.sin(a + 1.06),
-  );
-  CTX.lineTo(
-    // rear left
-    CVS.width / 2 +
-      ship.r * ((-1 / 3) * Math.cos(a + 1.06) + Math.sin(a + 1.06)),
-    CVS.height / 2 +
-      ship.r * ((-1 / 3) * Math.sin(a + 1.06) - Math.cos(a + 1.06)),
-  );
-  CTX.lineTo(
-    // rear right
-    CVS.width / 2 +
-      ship.r * ((-1 / 3) * Math.cos(a + 1.06) - Math.sin(a + 1.06)),
-    CVS.height / 2 +
-      ship.r * ((-1 / 3) * Math.sin(a + 1.06) + Math.cos(a + 1.06)),
-  );
+  CTX.moveTo(nose.x, nose.y);
+  CTX.lineTo(rearLeft.x, rearLeft.y);
+  CTX.lineTo(rearRight.x, rearRight.y);
   CTX.closePath();
   CTX.stroke();
 }
+
 /**
  * Draw the explosion when a ship is destroyed
  */
