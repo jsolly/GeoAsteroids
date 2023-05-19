ad995cb99117eecd321ec9a469fd707c6abfdec8
John Solly <jsolly@umd.edu>
2023-05-19 16:24:43 -0700

    remove ssr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vitest/config';
-import ssr from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
-  plugins: [ssr()],
   test: {
     environment: 'jsdom',
     threads: false,
