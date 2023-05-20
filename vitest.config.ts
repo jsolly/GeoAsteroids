4afa1d44b9975a4c96a34f188921f615c2d5a619
John Solly <jsolly@umd.edu>
2023-05-20 13:00:07 -0700

    Add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'c8',
+      reporter: ['lcov', 'text'],
+    },
     environment: 'jsdom',
     threads: false,
     setupFiles: 'setup/viteSetup.ts',
