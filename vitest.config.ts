aee101f60351743596b6925813148716142ba666
John Solly <jsolly@umd.edu>
2023-05-23 17:55:37 -0700

    exclude files from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,13 @@ export default defineConfig({
     coverage: {
       provider: 'c8',
       reporter: ['lcov', 'text'],
+      exclude: [
+        'src/asteroidsCanv.ts',
+        'src/lasersCanv.ts',
+        'src/shipCanv.ts',
+        'src/canvas.ts',
+        'test/**',
+      ],
     },
     environment: 'jsdom',
     threads: false,
