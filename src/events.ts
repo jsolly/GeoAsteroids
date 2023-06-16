a44aeb5817a979ba38f417b068252b9605a37b1d
John Solly <jsolly@umd.edu>
2023-06-15 17:33:45 -0700

    Add high scores modal

--- src/events.ts
+++ src/events.ts
@@ -3,99 +3,46 @@ import { setSound, setMusic } from './soundsMusic.js';
 import { setDifficulty, Difficulty, FPS } from './config.js';
 import { ship, newGame, update, currScore } from './main.js';
 
-document.addEventListener('keydown', (ev) => keyDown(ev, ship)); // pass ship to keyDown
-document.addEventListener('keyup', (ev) => keyUp(ev, ship)); // pass ship to keyUp
-
-let gameInterval: NodeJS.Timer;
-
-interface HighScore {
-  name: string;
-  score: number;
-}
-
-function startGame(): void {
-  newGame();
-  toggleScreen('start-screen', false);
-  toggleScreen('gameArea', true);
-  // Set up game loop
-  gameInterval = setInterval(update, 1000 / FPS);
-}
-
+const startGameBtn = getElementById<HTMLButtonElement>('start-game');
+const soundCheckBox = getElementById<HTMLInputElement>('soundPref');
+const musicCheckBox = getElementById<HTMLInputElement>('musicPref');
+const nameInput = getElementById<HTMLInputElement>('nameInput');
+const submitNameButton = getElementById<HTMLButtonElement>('submitNameButton');
+const highScoresButton = getElementById<HTMLButtonElement>(
+  'showHighScoresButton',
+);
+const highScoresList = getElementById<HTMLOListElement>('highScoresList');
 const difficultyButtonMap: Record<string, Difficulty> = {
   easy: Difficulty.easy,
   medium: Difficulty.medium,
   hard: Difficulty.hard,
 };
 
-function toggleScreen(id: string, toggle: boolean): void {
-  const element = getElementById<HTMLElement>(id);
-  if (element) {
-    element.style.display = toggle ? 'block' : 'none';
-  }
+document.addEventListener('keydown', (ev) => keyDown(ev, ship)); // pass ship to keyDown
+document.addEventListener('keyup', (ev) => keyUp(ev, ship)); // pass ship to keyUp
+
+attachEventListener(highScoresButton, 'click', fetchHighScores);
+attachEventListener(startGameBtn, 'click', startGame);
+attachEventListener(soundCheckBox, 'change', (ev) => {
+  const target = ev.target as HTMLInputElement;
+  setSound(target.checked);
+});
+
+attachEventListener(musicCheckBox, 'change', (ev) => {
+  const target = ev.target as HTMLInputElement;
+  setMusic(target.checked);
+});
+
+if (nameInput && submitNameButton) {
+  attachEventListener(nameInput, 'input', () => validateInput(nameInput));
+  attachEventListener(submitNameButton, 'click', submitName);
 }
 
-async function postHighScore(highScore: HighScore): Promise<void> {
-  try {
-    const response = await fetch('/api/highscores', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(highScore),
-    });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-  } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
-  }
-}
+let gameInterval: NodeJS.Timer;
 
-function validateInput(input: HTMLInputElement): void {
-  input.value = input.value.replace(/[^A-Za-z0-9]/g, '');
-}
-
-function showGameOverMenu(): void {
-  clearInterval(gameInterval); // Stop the game loop
-
-  // Show the game over modal
-  const gameOverModal = getElementById<HTMLElement>('gameOverModal');
-  if (gameOverModal) {
-    gameOverModal.style.display = 'block';
-  }
-}
-
-async function submitName(): Promise<void> {
-  const nameInput = getElementById<HTMLInputElement>('nameInput');
-  if (!nameInput) {
-    return;
-  }
-
-  validateInput(nameInput);
-  const name = nameInput.value;
-
-  // Call Serverside API to save score
-  const highScore: HighScore = { name, currScore };
-  await postHighScore(highScore);
-
-  // Hide the game over modal
-  const gameOverModal = getElementById<HTMLElement>('gameOverModal');
-  if (gameOverModal) {
-    gameOverModal.style.display = 'none';
-  }
-
-  // Clear the input field for the next game
-  nameInput.value = '';
-
-  newGame();
-  clearInterval(gameInterval);
-  const startGameBtn = getElementById<HTMLButtonElement>('start-game');
-  if (startGameBtn) {
-    startGameBtn.innerText = 'Play Again! 🚀';
-  }
-
-  toggleScreen('start-screen', true);
-  toggleScreen('gameArea', false);
+interface HighScore {
+  name: string;
+  score: number;
 }
 
 function getElementById<T extends HTMLElement>(id: string): T | null {
@@ -125,26 +72,8 @@ function attachEventListener<T extends HTMLElement>(
   }
 }
 
-const startGameBtn = getElementById<HTMLButtonElement>('start-game');
-const soundCheckBox = getElementById<HTMLInputElement>('soundPref');
-const musicCheckBox = getElementById<HTMLInputElement>('musicPref');
-const nameInput = getElementById<HTMLInputElement>('nameInput');
-const submitNameButton = getElementById<HTMLButtonElement>('submitNameButton');
-
-attachEventListener(startGameBtn, 'click', startGame);
-attachEventListener(soundCheckBox, 'change', (ev) => {
-  const target = ev.target as HTMLInputElement;
-  setSound(target.checked);
-});
-
-attachEventListener(musicCheckBox, 'change', (ev) => {
-  const target = ev.target as HTMLInputElement;
-  setMusic(target.checked);
-});
-
-if (nameInput && submitNameButton) {
-  attachEventListener(nameInput, 'input', () => validateInput(nameInput));
-  attachEventListener(submitNameButton, 'click', submitName);
+function validateInput(input: HTMLInputElement): void {
+  input.value = input.value.replace(/[^A-Za-z0-9]/g, '');
 }
 
 Object.entries(difficultyButtonMap).forEach(([id, difficulty]) => {
@@ -156,5 +85,108 @@ Object.entries(difficultyButtonMap).forEach(([id, difficulty]) => {
     }
   });
 });
+
+function startGame(): void {
+  newGame();
+  toggleScreen('start-screen', false);
+  toggleScreen('gameArea', true);
+  // Set up game loop
+  gameInterval = setInterval(update, 1000 / FPS);
+}
+
+function toggleScreen(id: string, toggle: boolean): void {
+  const element = getElementById<HTMLElement>(id);
+  if (element) {
+    element.style.display = toggle ? 'block' : 'none';
+  }
+}
+
+async function postHighScore(highScore: HighScore): Promise<void> {
+  try {
+    const response = await fetch('/api/highscores', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(highScore),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+function showGameOverMenu(): void {
+  clearInterval(gameInterval); // Stop the game loop
+
+  // Show the game over modal
+  const gameOverModal = getElementById<HTMLElement>('gameOverModal');
+  if (gameOverModal) {
+    gameOverModal.style.display = 'block';
+  }
+}
+
+async function submitName(): Promise<void> {
+  const nameInput = getElementById<HTMLInputElement>('nameInput');
+  if (!nameInput) {
+    return;
+  }
+
+  validateInput(nameInput);
+  const name = nameInput.value;
+  const score = currScore;
+
+  // Call Serverside API to save score
+  const highScore: HighScore = { name, score };
+  await postHighScore(highScore);
+
+  // Hide the game over modal
+  const gameOverModal = getElementById<HTMLElement>('gameOverModal');
+  if (gameOverModal) {
+    gameOverModal.style.display = 'none';
+  }
+
+  // Clear the input field for the next game
+  nameInput.value = '';
+
+  newGame();
+  clearInterval(gameInterval);
+  const startGameBtn = getElementById<HTMLButtonElement>('start-game');
+  if (startGameBtn) {
+    startGameBtn.innerText = 'Play Again! 🚀';
+  }
+
+  toggleScreen('start-screen', true);
+  toggleScreen('gameArea', false);
+}
+interface HighScoresResponse {
+  highScores: HighScore[];
+}
+
+async function fetchHighScores(): Promise<void> {
+  try {
+    const response = await fetch('/api/highscores');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = (await response.json()) as HighScoresResponse;
+
+    if (highScoresList) {
+      // Clear any existing scores
+      highScoresList.innerHTML = '';
+
+      // Add new scores
+      data.highScores.forEach((score) => {
+        const li = document.createElement('li');
+        li.textContent = `${score.name}: ${score.score}`;
+        highScoresList.appendChild(li);
+      });
+    }
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
 
 export { showGameOverMenu };
